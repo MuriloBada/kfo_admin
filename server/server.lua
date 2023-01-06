3ecac942c77af814e6d29fb55525ec760b7e105d
Murilo Bada <murilomaffiolettibada@hotmail.com>
2023-01-06 20:56:45 -0300

    Added removeped command

--- server/server.lua
+++ server/server.lua
@@ -39,6 +39,25 @@ RegisterCommand('setped', function(source, args, rawCommand)
     end)
 end)
 
+RegisterCommand('removeped', function(source, args, rawCommand)
+    local _source = source
+    TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
+        if exports.kfo_permissions.checkPlayerJob(_source, 'Admin', user.getIdentifier(), user.getSessionVar('charid')) then
+            if args[1] and args[2] then
+                local skins = MySQL.Sync.fetchAll('SELECT * FROM SKINS WHERE identifier = @identifier and charid = @charid', {identifier = args[1], charid = tonumber(args[2])})
+                local temp = json.decode(skins[1].skin)
+                temp.model = ""
+                MySQL.Sync.execute('UPDATE skins set skin = @fSkin WHERE identifier = @identifier and charid = @charid', {fSkin = json.encode(temp), identifier = args[1], charid = tonumber(args[2])})
+                TriggerClientEvent('redem_roleplay:Tip', _source, "Ped removido da hex "..args[1]..' [ '..args[2]..' ]', 7000)
+            else
+                TriggerClientEvent('redem_roleplay:Tip', _source, "Você deve usar /removeped [steam hex] [charid]", 7000)
+            end
+        else
+            TriggerClientEvent('redem_roleplay:Tip', _source, "Você não tem permissão para acessar este comando.", 7000)
+        end
+    end)
+end)
+
 RegisterCommand('outfit', function(source, args)
     TriggerClientEvent('kfo_admin:outfitPreset', source, args[1])
 end)
